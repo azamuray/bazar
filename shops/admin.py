b02c0dc800b0bb1d01d329013dc6710a14104c26
ai-rage <azamat.murdalov@mail.ru>
2019-06-07 01:27:12 +0300

    add Shop model to the shops app

--- shops/admin.py
+++ shops/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Shop
+
+admin.site.register(Shop)
--- shops/models.py
+++ shops/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Shop(models.Model):
+	seller = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Продавец")
+	name = models.CharField("Название", max_length=120)
+	address = models.CharField("Адрес", max_length=200)
+	phone = models.CharField("Телефон", max_length=30)
+
+	class Meta:
+		verbose_name = 'магазин'
+		verbose_name_plural = 'Магазины'
+
+	def __str__(self):
+		return self.name
