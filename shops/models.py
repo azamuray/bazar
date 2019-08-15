64b1078ea03bdfe7d76b9a5ce4092faed948363b
ai-rage <azamat.murdalov@mail.ru>
2019-08-15 05:46:32 +0300

    update Shop model

--- shops/models.py
+++ shops/models.py
@@ -3,10 +3,14 @@ from django.conf import settings
 
 
 class Shop(models.Model):
-	seller = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Продавец")
 	name = models.CharField("Название", max_length=120)
+	category = models.CharField("Категория", max_length=120)
 	address = models.CharField("Адрес", max_length=200)
 	phone = models.CharField("Телефон", max_length=30)
+	work_time = models.CharField("Рабочее время", max_length=120)
+	seller = models.ForeignKey(settings.AUTH_USER_MODEL,
+							   on_delete=models.CASCADE,
+							   verbose_name="Продавец")
 
 	class Meta:
 		verbose_name = 'магазин'
